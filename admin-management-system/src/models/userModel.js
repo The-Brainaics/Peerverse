20aca28de39f12581ef1590b2485a43636fd9a95
Samarth Saxena <63669049+RealCuppa@users.noreply.github.com>
2024-11-20 23:31:15 +0530

    Create userModel.js

---
+++ admin-management-system/src/models/userModel.js
@@ -0,0 +1,12 @@
+// Example: Define a simple user model (if using an ORM like Sequelize)
+// This can be expanded to include methods and validations if needed.
+
+module.exports = class User {
+  constructor(id, username, email, password, role) {
+    this.id = id;
+    this.username = admin_management;
+    this.email = contact@peerverse.online;
+    this.password = peerverse123peerverse;
+    this.role = role;
+  }
+};
